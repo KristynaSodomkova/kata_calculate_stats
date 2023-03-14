d00264cbe6f999166c0d5e28a96bb112b9b5a4ea
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-14 22:35:52 +0100

    modified test for minimal value

--- src/kata_calculate_stats/stats.py
+++ src/kata_calculate_stats/stats.py
@@ -1,4 +1,6 @@
 class Stats:
 
-    def __init__(self, list_of_numbers):
-        self.list_of_numbers = list_of_numbers
+    def __init__(self):
+        pass
+
+
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,9 +1,12 @@
 from kata_calculate_stats.stats import Stats
 
-
+example_list = [6, 9, 15, -2, 92, 11]
 # test that a list named list_of_numbers exists
 def test_min_value():
-    assert Stats.list_of_numbers
+    stats = Stats()
+    var_min_value = stats.min_value(example_list)
+
+    assert var_min_value == -2
 
 
 
