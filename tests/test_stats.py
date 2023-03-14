8cfbb42cf80587649b341d1525591e3d998a5a10
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-14 22:49:20 +0100

    successfully failed test for maximum value

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -6,6 +6,8 @@ def test_min_value():
     stats = Stats()
     assert stats.min_value(example_list) == -2
 
-
+def test_max_value():
+    stats = Stats()
+    assert stats.max_value(example_list) == 92
 
 
