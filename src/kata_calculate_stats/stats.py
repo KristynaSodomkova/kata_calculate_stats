044b340252762f2abd375b57e3ec0b35667f3f99
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-14 23:06:29 +0100

    test for average_value() passed

--- src/kata_calculate_stats/stats.py
+++ src/kata_calculate_stats/stats.py
@@ -14,3 +14,9 @@ class Stats:
     @staticmethod
     def num_of_elements(list_of_num):
         return len(list_of_num)
+
+    @staticmethod
+    def average_value(list_of_num):
+        num_of_el = len(list_of_num)
+        sum_of_el = sum(list_of_num)
+        return round(sum_of_el/num_of_el, 6)
