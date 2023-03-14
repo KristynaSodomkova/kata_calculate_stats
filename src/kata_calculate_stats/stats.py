b4a596fa99f0a702eef1ac6929cdc2221714b5c5
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-14 22:48:11 +0100

    min_value method successfully tested

--- src/kata_calculate_stats/stats.py
+++ src/kata_calculate_stats/stats.py
@@ -3,4 +3,7 @@ class Stats:
     def __init__(self):
         pass
 
+    @staticmethod
+    def min_value(list_of_num):
+        return min(list_of_num)
 
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -4,9 +4,7 @@ example_list = [6, 9, 15, -2, 92, 11]
 # test that a list named list_of_numbers exists
 def test_min_value():
     stats = Stats()
-    var_min_value = stats.min_value(example_list)
-
-    assert var_min_value == -2
+    assert stats.min_value(example_list) == -2
 
 
 
